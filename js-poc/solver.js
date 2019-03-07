dc06045cc2c48137a3c89fb07b00107d593f6c54
Maciej Krüger <mkg20001@gmail.com>
2019-03-07 17:30:25 +0100

    feat: continue work on js poc

--- js-poc/solver.js
+++ js-poc/solver.js
@@ -3,6 +3,7 @@
 const fetch = require('node-fetch')
 const yaml = require('js-yaml')
 const fs = require('fs')
+const { URLSearchParams } = require('url')
 
 const dlv = require('dlv')
 const dset = require('dset')
@@ -22,4 +23,130 @@ function matcher (variables, key, matchWith) {
   let matchType = key.pop()
   let value = dlv(variables, key)
   return Matchers[matchType](value, matchWith)
+}
+
+const templateParams = {
+  header: {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
+  }
+}
+
+async function turnResponseIntoData (params, res) {
+  let rawHeaders = res.headers.raw()
+  for (const k in rawHeaders) { // eslinr-disable-line guard-for-in
+    rawHeaders[k] = rawHeaders[k][0]
+  }
+
+  return {
+    url: res.url,
+    // TODO: query,
+    header: rawHeaders,
+    redirect: res.status >= 300 && res.status < 400,
+    status: res.status,
+    method: params.method
+  }
+}
+
+const kickoffParameters = {
+  url: 'http://detectportal.firefox.com'
+}
+
+async function doRequest ({jar}, {url, method, setCookie, clearCookie, formdata, jsondata, header}) {
+  let params = clone(templateParams)
+
+  params.url = url
+
+  params.method = method || 'GET'
+
+  for (const cookie in setCookie) {
+    // TODO: jar.set(cookie, setCookie[cookie])
+  }
+
+  if (clearCookie) {
+    clearCookie.forEach(cookie => {
+      // TODO: jar.delete(cookie)
+    })
+  }
+
+  // TODO: params.headers.Cookie = jar.getCookieForURL(url)
+
+  if (formdata) {
+    params.body = new URLSearchParams(formdata)
+  }
+
+  if (jsondata) {
+    params.body = JSON.stringify(jsondata)
+    params.headers['Content-Type'] = 'application/json'
+  }
+
+  const res = await fetch(url, params)
+
+  return turnResponseIntoData(params, res)
+}
+
+function generateShared () {
+  // TODO: jar
+  return {}
+}
+
+function compareCondition (variables, condition) {
+  for (const key in condition) {
+    if (!matcher(variables, key, condition[key])) {
+      return false
+    }
+  }
+
+  return true
+}
+
+async function main () {
+  const shared = generateShared()
+  const firstRes = await doRequest(shared, kickoffParameters)
+
+  // TODO: load config
+  const selectedPortal = Portals.filter(portal => compareCondition(firstRes, portal.solution.main.match))[0]
+
+  let ops = selectedPortal.solution
+  let op = ops.main
+  let res = firstRes
+
+  while (true) {
+    if (op.respond) {
+      res = await doRequest(shared, op.respond)
+      if (op.continueTo) {
+        op = ops[op.continueTo]
+      } else {
+        for (const subOpId in ops) {
+          const subOp = ops[subOpId]
+          if (compareCondition(res, subOp.match)) {
+            op = subOp
+            break
+          }
+        }
+      }
+    } else if (op.action) {
+      switch (op.action) {
+        case 'failure': {
+          throw new Error('General Failure')
+        }
+        case 'invalidCredentials': {
+          throw new Error('Invalid credentials supplied by user')
+        }
+        case 'success': {
+          // TODO: bail
+        }
+        case 'continueMatch': {
+          for (const subOpId in ops) {
+            const subOp = ops[subOpId]
+            if (compareCondition(res, subOp.match)) {
+              op = subOp
+              break
+            }
+          }
+          break
+        }
+        default: throw new TypeError(op.action)
+      }
+    }
+  }
 }
